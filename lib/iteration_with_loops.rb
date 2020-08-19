46e8ac34ef21bad77c81da2b74b2853ef8e9d87a
Jon Noriega <noriega331@gmail.com>
2020-08-19 22:44:19 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,7 @@ def find_min_in_nested_arrays(src)
   
   while row_index < src.length do
     element_index = 0
-    min_daily_temp = 150
+    min_daily_temp = ""
     while element_index < src[row_index].length do
       if src[row_index][element_index] < min_daily_temp
         min_daily_temp = src[row_index][element_index]
